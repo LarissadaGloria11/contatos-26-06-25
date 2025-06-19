3daadc45e2dbcf23717f2d519b85b72c10f4bb04
danielvalmeida91 <danielvalmeida91@gmail.com>
2025-06-18 21:27:35 -0300

    addd apiKey auth

--- routes/contatoRoutes.js
+++ routes/contatoRoutes.js
@@ -4,6 +4,7 @@ const Contatos = require('../models/Contato');
 const { default: mongoose } = require("mongoose");
 
 const basicAuth = require('../middlewares/basicAuth')
+const apiKeyAuth = require('../middlewares/apiKeyAuth')
 
 // Listar contatos
 router.get('/', basicAuth, async (req, res) => {
@@ -18,14 +19,8 @@ router.get('/', basicAuth, async (req, res) => {
     res.json(retorno)
 })
 
-// Criar contato
-router.post('/', async (req, res) => {
-    const dados = await Contatos.create(req.body);
-    res.status(200).json(dados)
-})
-
 //Buscar por ID
-router.get('/:id', async (req, res) => {
+router.get('/:id', apiKeyAuth, async (req, res) => {
     try {
         if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
             return res.status(404).json({ "error": "Id enviado não é válido" })
@@ -40,6 +35,14 @@ router.get('/:id', async (req, res) => {
         res.status(404).json({ "error": err.message })
     }
 })
+
+
+// Criar contato
+router.post('/', async (req, res) => {
+    const dados = await Contatos.create(req.body);
+    res.status(200).json(dados)
+})
+
 
 //Atualizar por ID
 router.put('/:id', async (req, res) => {
