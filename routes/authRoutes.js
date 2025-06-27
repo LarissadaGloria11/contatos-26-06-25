130e7cc99807eafd2349bd49092c6e443f637744
LarissaGloria <0000094220@senaimgaluno.com.br>
2025-06-26 22:25:07 -0300

    Aula26-06-2025

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -20,7 +20,49 @@ router.post('/login', async (req, res) => {
         return res.status(401).json('Senha inválida')
     }
 
+    const token = JWT.sign({id:dado._id, email:dado.email}, process.env.JWT_SECRET, {
+        expresIn: '1h'
+    })
+
+    res.json(token)
+
+
     console.log('logado!!!')
+
+})
+
+router.post('/register', async (req, res) => {
+
+    //receber email e senha
+    const { email, senha } = req.body
+
+    //verificar se email não existe no banco
+
+    const usuarioExiste = await Usuario.findOne({ email })
+    console.log(usuarioExiste)
+    if (usuarioExiste) {
+        return res.status(400).json('Email já cadastrado')
+
+    }
+
+    // criptografar a senha
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+    console.log(senhaCriptografada)
+
+    //salvar dados no banco
+
+    const novoUsuario = new Usuario({email, senha:senhaCriptografada})
+    await novoUsuario.save()
+
+     //apresentar resultado
+
+    res.status(201).json('Usuário Cadastrado com sucesso')
+   
+
+
+
 
 })
 
